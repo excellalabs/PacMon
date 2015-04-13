7fba86c1450c4740b7fef35eb9a1ddbc87ab0f9b
eskelley <erland.kelley@gmail.com>
2015-04-13 13:04:24 -0400

    Renamed functions according to PowerShell convention

--- pacmon.ps1
+++ pacmon.ps1
@@ -8,7 +8,10 @@ Param(
 	[string]$target,
 	
 	[Parameter(Mandatory=$FALSE)]
-	[string]$java="java",
+	[string]$java="java",	
+	
+	[Parameter(Mandatory=$FALSE)]
+	[string]$opts,
 	
 	[Parameter(Mandatory=$FALSE)]
 	[string]$dc="dc",
@@ -17,54 +20,84 @@ Param(
 	[string]$s = "suppress.xml",
 	
 	[Parameter(Mandatory=$FALSE)]
-	[string]$x = 'output.xml',
+	[string]$x = "output.xml",
 	
 	[Parameter(Mandatory=$FALSE)]
-	[string]$h = 'vulnerabilities.html'
+	[string]$h = "vulnerabilities.html"
 )
 
 ### END INIT PARAMS
 
-function parseDependencies($dependencies) {
+function Run-DependencyCheck([string]$javaCmd, [string]$dcPath, [string]$cmdLineArgs){
+	[string]$repoPath = '{0}\repo' -f $dcPath
+	[string]$classPath = '"{0}"\etc;"{1}"\commons-cli\commons-cli\1.2\commons-cli-1.2.jar;"{1}"\org\owasp\dependency-check-core\1.2.9\dependency-check-core-1.2.9.jar;"{1}"\org\apache\commons\commons-compress\1.9\commons-compress-1.9.jar;"{1}"\commons-io\commons-io\2.4\commons-io-2.4.jar;"{1}"\commons-lang\commons-lang\2.6\commons-lang-2.6.jar;"{1}"\org\apache\lucene\lucene-core\4.7.2\lucene-core-4.7.2.jar;"{1}"\org\apache\lucene\lucene-analyzers-common\4.7.2\lucene-analyzers-common-4.7.2.jar;"{1}"\org\apache\lucene\lucene-queryparser\4.7.2\lucene-queryparser-4.7.2.jar;"{1}"\org\apache\lucene\lucene-queries\4.7.2\lucene-queries-4.7.2.jar;"{1}"\org\apache\lucene\lucene-sandbox\4.7.2\lucene-sandbox-4.7.2.jar;"{1}"\org\apache\velocity\velocity\1.7\velocity-1.7.jar;"{1}"\commons-collections\commons-collections\3.2.1\commons-collections-3.2.1.jar;"{1}"\com\h2database\h2\1.3.176\h2-1.3.176.jar;"{1}"\org\jsoup\jsoup\1.7.2\jsoup-1.7.2.jar;"{1}"\org\owasp\dependency-check-utils\1.2.9\dependency-check-utils-1.2.9.jar;"{1}"\org\owasp\dependency-check-cli\1.2.9\dependency-check-cli-1.2.9.jar' -f $dcPath, $repoPath						  
+	$command = '{0} -classpath {1} -Dapp.name="dependency-check" -Dapp.repo="{2}" -Dapp.home="{3}" -Dbasedir="{3}" org.owasp.dependencycheck.App {4}' -f $javaCmd, $classPath, $repoPath, $dcPath, $cmdLineArgs
+	cmd.exe /C $command
+}
+
+function Get-Dependencies([string]$xmlPath) {
+	if (!(Test-Path $xmlPath)) {
+		Write-Error ("XML output not found: {0}" -f $xmlPath)
+		exit(1)
+	}
+
+	[xml]$xml = Get-Content $xmlPath	
+
+	if (!$xml.analysis) {
+		Write-Error "XML contains no analysis"
+		Invoke-Expression ('DEL {0}' -f $xmlPath)
+		exit(1)
+	}
+	
+	if (!$xml.analysis.dependencies.dependency) {
+		Write-Error "Analysis contains no dependencies"
+		Invoke-Expression ('DEL {0}' -f $xmlPath)
+		exit(0)
+	}
+	
+	$xml.analysis.dependencies.dependency
+}
+
+function Parse-Dependencies($dependencies) {
 	Foreach ($dependency IN $dependencies) {
-		parseDependency($dependency)
+		Parse-Dependency($dependency)
 	}
 }
 
-function parseDependency($dependency) {
-	[string]$name = cleanString($dependency.fileName)
-	[string]$description = cleanString($dependency.description)
+function Parse-Dependency($dependency) {
+	[string]$name = Clean-String($dependency.fileName)
+	[string]$description = Clean-String($dependency.description)
 	$vulnerabilities = $dependency.vulnerabilities.vulnerability
 	
-	startTest $name
+	Start-Test $name
 	
 	if ($description) {
-		updateTest $name $description
+		Update-Test $name $description
 	}
 	
 	if ($dependency.vulnerabilities) {
-		parseVulnerabilities $name $vulnerabilities
+		Parse-Vulnerabilities $name $vulnerabilities
 	}
 	
-	endTest($name)
+	End-Test($name)
 }
 
-function parseVulnerabilities([string]$name, $vulnerabilities){
+function Parse-Vulnerabilities([string]$name, $vulnerabilities){
 	Foreach ($vulnerability in $vulnerabilities) {
-		parseVulnerability $name $vulnerability
+		Parse-Vulnerability $name $vulnerability
 	}
 }
 
-function parseVulnerability([string]$name, $vulnerability){
-	[string]$vulnerabilityName = cleanString($vulnerability.name)
-	[string]$vulnerabilitySeverity = cleanString($vulnerability.severity)
+function Parse-Vulnerability([string]$name, $vulnerability){
+	[string]$vulnerabilityName = Clean-String($vulnerability.name)
+	[string]$vulnerabilitySeverity = Clean-String($vulnerability.severity)
 	[string]$message = "{0} ({1})" -f $vulnerabilityName, $vulnerabilitySeverity
-	[string]$details = cleanString($vulnerability.description)
+	[string]$details = Clean-String($vulnerability.description)
 	
-	failTest $name $message $details
+	Fail-Test $name $message $details
 }
 
-function hasVulnerability($dependencies) {
+function Has-Vulnerability($dependencies) {
 	$vulnerabilityFound = $FALSE
 	Foreach ($dependency IN $dependencies) {
 		if ($dependency.vulnerabilities) {
@@ -74,38 +107,27 @@ function hasVulnerability($dependencies) {
 	$vulnerabilityFound
 }
 
-function startTest([string]$name){
-	Write-Output ("##teamcity[testStarted name='{0}']" -f $name)
-}
-
-function updateTest([string]$name, [string]$text){
-	Write-Output ("##teamcity[testStdOut name='{0}' out='{1}']" -f $name, $text)
-}
-
-function failTest([string]$name, [string]$message, [string]$details){
-	Write-Output ("##teamcity[testFailed name='{0}' message='{1}' details='{2}']" -f $name, $message, $details)
-}
-
-function endTest([string]$name){
-	Write-Output ("##teamcity[testFinished name='{0}']" -f $name)
-}
-
-function cleanString([string]$string){
+function Clean-String([string]$string){
 	$string = $string -replace "`t|`n|`r",""
 	$string = $string -replace " ;|; ",";"
 	$string = $string -replace "'",""
 	$string
 }
 
-function executeDependencyCheck([string]$javaCmd, [string]$dcPath, [string]$cmdLineArgs){
-	[string]$repoPath = '{0}\repo' -f $dcPath
-	
-	# set classpath
-	[string]$classPath = '"{0}"\etc;"{1}"\commons-cli\commons-cli\1.2\commons-cli-1.2.jar;"{1}"\org\owasp\dependency-check-core\1.2.9\dependency-check-core-1.2.9.jar;"{1}"\org\apache\commons\commons-compress\1.9\commons-compress-1.9.jar;"{1}"\commons-io\commons-io\2.4\commons-io-2.4.jar;"{1}"\commons-lang\commons-lang\2.6\commons-lang-2.6.jar;"{1}"\org\apache\lucene\lucene-core\4.7.2\lucene-core-4.7.2.jar;"{1}"\org\apache\lucene\lucene-analyzers-common\4.7.2\lucene-analyzers-common-4.7.2.jar;"{1}"\org\apache\lucene\lucene-queryparser\4.7.2\lucene-queryparser-4.7.2.jar;"{1}"\org\apache\lucene\lucene-queries\4.7.2\lucene-queries-4.7.2.jar;"{1}"\org\apache\lucene\lucene-sandbox\4.7.2\lucene-sandbox-4.7.2.jar;"{1}"\org\apache\velocity\velocity\1.7\velocity-1.7.jar;"{1}"\commons-collections\commons-collections\3.2.1\commons-collections-3.2.1.jar;"{1}"\com\h2database\h2\1.3.176\h2-1.3.176.jar;"{1}"\org\jsoup\jsoup\1.7.2\jsoup-1.7.2.jar;"{1}"\org\owasp\dependency-check-utils\1.2.9\dependency-check-utils-1.2.9.jar;"{1}"\org\owasp\dependency-check-cli\1.2.9\dependency-check-cli-1.2.9.jar' -f $dcPath, $repoPath
-	
-	$command = '{0} -classpath {1} -Dapp.name="dependency-check" -Dapp.repo="{2}" -Dapp.home="{3}" -Dbasedir="{3}" org.owasp.dependencycheck.App {4}' -f $javaCmd, $classPath, $repoPath, $dcPath, $cmdLineArgs
-	Write-Output ("Executing cmd.exe /C {0}" -f $command)
-	cmd.exe /C $command
+function Start-Test([string]$name){
+	Write-Output ("##teamcity[testStarted name='{0}']" -f $name)
+}
+
+function Update-Test([string]$name, [string]$text){
+	Write-Output ("##teamcity[testStdOut name='{0}' out='{1}']" -f $name, $text)
+}
+
+function Fail-Test([string]$name, [string]$message, [string]$details){
+	Write-Output ("##teamcity[testFailed name='{0}' message='{1}' details='{2}']" -f $name, $message, $details)
+}
+
+function End-Test([string]$name){
+	Write-Output ("##teamcity[testFinished name='{0}']" -f $name)
 }
 
 #
@@ -145,6 +167,8 @@ function Set-PSConsole {
 [string]$htmlPath = '{0}\{1}' -f $basePath, $htmlFilename
 [string]$suppressPath = '{0}\{1}' -f $basePath, $suppressFilename
 
+[string]$javaCmd = '{0} {1}' -f $java, $opts
+
 if (Test-Path $suppressPath) {
 	[string]$scanArgs = '-a "VulnerabilityScan" -s "{0}" -o "{1}" -f "XML" --suppression "{2}"' -f $inputPath, $xmlPath, $suppressPath
 	[string]$artifactArgs = '-a "VulnerabilityScan" -s "{0}" -o "{1}" -f "HTML" --suppression "{2}"' -f $inputPath, $htmlPath, $suppressPath
@@ -153,40 +177,19 @@ if (Test-Path $suppressPath) {
 	[string]$artifactArgs = '-a "VulnerabilityScan" -s "{0}" -o "{1}" -f "HTML"' -f $inputPath, $htmlPath
 }
 
-executeDependencyCheck $java $dcPath $scanArgs
+Run-DependencyCheck $javaCmd $dcPath $scanArgs
 
-if (Test-Path $xmlPath) {
-	Write-Output ("Parsing XML output: {0}" -f $xmlPath)
-} else {
-	Write-Error ("XML output not found: {0}" -f $xmlPath)
-	exit(1)
-}
-
-[xml]$xml = Get-Content $xmlPath	
-
-if (!$xml.analysis) {
-	Write-Error "XML contains no analysis"
-	Invoke-Expression ('DEL {0}' -f $xmlPath)
-	exit(1)
-}
-
-$dependencies = $xml.analysis.dependencies.dependency
-
-if (!$dependencies) {
-	Write-Error "Analysis contains no dependencies"
-	Invoke-Expression ('DEL {0}' -f $xmlPath)
-	exit(0)
-}
+$dependencies = Get-Dependencies $xmlPath
 
 Set-PSConsole
 
-parseDependencies $dependencies
+Parse-Dependencies $dependencies
 
 Invoke-Expression ('DEL {0}' -f $xmlPath)
 
-if (hasVulnerability $dependencies) {
+if (Has-Vulnerability $dependencies) {
 	Write-Output ("Vulnerability found -- generating report artifact: {0}" -f $htmlFilename)
-	executeDependencyCheck $java $dcPath $artifactArgs
+	Run-DependencyCheck $javaCmd $dcPath $artifactArgs
 }
 
 exit(0)
