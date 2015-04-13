079b8f2fcb6cec52ac411fca120b8dbb42faa612
eskelley <erland.kelley@gmail.com>
2015-04-13 12:37:19 -0400

    Parameterized PS script, moved java call inside, added empty suppression file

--- pacmon.cmd
+++ pacmon.cmd
@@ -1,1 +1,1 @@
-CALL PowerShell.exe .\pacmon.ps1 %1
+CALL PowerShell.exe .\pacmon.ps1 -target "%*"
--- pacmon.ps1
+++ pacmon.ps1
@@ -1,5 +1,30 @@
 # PacMon - Dependency Check Runner for TeamCity
 
+### BEGIN INIT PARAMS
+
+[CmdletBinding()]
+Param(
+	[Parameter(Mandatory=$TRUE)]
+	[string]$target,
+	
+	[Parameter(Mandatory=$FALSE)]
+	[string]$java="java",
+	
+	[Parameter(Mandatory=$FALSE)]
+	[string]$dc="dc",
+	
+	[Parameter(Mandatory=$FALSE)]
+	[string]$s = "suppress.xml",
+	
+	[Parameter(Mandatory=$FALSE)]
+	[string]$x = 'output.xml',
+	
+	[Parameter(Mandatory=$FALSE)]
+	[string]$h = 'vulnerabilities.html'
+)
+
+### END INIT PARAMS
+
 function parseDependencies($dependencies) {
 	Foreach ($dependency IN $dependencies) {
 		parseDependency($dependency)
@@ -72,30 +97,29 @@ function cleanString([string]$string){
 	$string
 }
 
-function executeDependencyCheck([string]$dcPath, [string]$cmdLineArgs){
-	[string]$javaCmd = 'java'
-	[string]$javaOpts = ''
-	
+function executeDependencyCheck([string]$javaCmd, [string]$dcPath, [string]$cmdLineArgs){
 	[string]$repoPath = '{0}\repo' -f $dcPath
 	
 	# set classpath
 	[string]$classPath = '"{0}"\etc;"{1}"\commons-cli\commons-cli\1.2\commons-cli-1.2.jar;"{1}"\org\owasp\dependency-check-core\1.2.9\dependency-check-core-1.2.9.jar;"{1}"\org\apache\commons\commons-compress\1.9\commons-compress-1.9.jar;"{1}"\commons-io\commons-io\2.4\commons-io-2.4.jar;"{1}"\commons-lang\commons-lang\2.6\commons-lang-2.6.jar;"{1}"\org\apache\lucene\lucene-core\4.7.2\lucene-core-4.7.2.jar;"{1}"\org\apache\lucene\lucene-analyzers-common\4.7.2\lucene-analyzers-common-4.7.2.jar;"{1}"\org\apache\lucene\lucene-queryparser\4.7.2\lucene-queryparser-4.7.2.jar;"{1}"\org\apache\lucene\lucene-queries\4.7.2\lucene-queries-4.7.2.jar;"{1}"\org\apache\lucene\lucene-sandbox\4.7.2\lucene-sandbox-4.7.2.jar;"{1}"\org\apache\velocity\velocity\1.7\velocity-1.7.jar;"{1}"\commons-collections\commons-collections\3.2.1\commons-collections-3.2.1.jar;"{1}"\com\h2database\h2\1.3.176\h2-1.3.176.jar;"{1}"\org\jsoup\jsoup\1.7.2\jsoup-1.7.2.jar;"{1}"\org\owasp\dependency-check-utils\1.2.9\dependency-check-utils-1.2.9.jar;"{1}"\org\owasp\dependency-check-cli\1.2.9\dependency-check-cli-1.2.9.jar' -f $dcPath, $repoPath
-	set-item -path Env:CLASSPATH -value $classPath
-	"CLASSPATH = $Env:CLASSPATH" 
 	
-	$command = '{0} {1} -classpath {2} -Dapp.name="dependency-check" -Dapp.repo="{3}" -Dapp.home="{4}" -Dbasedir="{4}" org.owasp.dependencycheck.App {5}' -f $javaCmd, $javaOpts, $classPath, $repoPath, $dcPath, $cmdLineArgs
+	$command = '{0} -classpath {1} -Dapp.name="dependency-check" -Dapp.repo="{2}" -Dapp.home="{3}" -Dbasedir="{3}" org.owasp.dependencycheck.App {4}' -f $javaCmd, $classPath, $repoPath, $dcPath, $cmdLineArgs
 	Write-Output ("Executing cmd.exe /C {0}" -f $command)
 	cmd.exe /C $command
 }
 
-#http://stackoverflow.com/questions/1183183/path-of-currently-executing-powershell-script
+#
+# http://stackoverflow.com/questions/1183183/path-of-currently-executing-powershell-script
+#
 function Get-ScriptDirectory
 {
 	$Invocation = (Get-Variable MyInvocation -Scope 1).Value
 	Split-Path $Invocation.MyCommand.Path
 }
 
-#https://confluence.jetbrains.com/display/TCD9/PowerShell
+#
+# https://confluence.jetbrains.com/display/TCD9/PowerShell
+#
 function Set-PSConsole {
 	if (Test-Path env:TEAMCITY_VERSION) {
 		try {
@@ -108,22 +132,28 @@ function Set-PSConsole {
 }
 
 ### BEGIN SCRIPT
-[string]$dcRelPath = 'dc'
-[string]$xmlFilename = 'output.xml'
-[string]$htmlFilename = 'vulnerabilities.html'
-[string]$inputRelPath = '.\Lambchop'
+
+[string]$suppressFilename = $s
+[string]$xmlFilename = $x
+[string]$htmlFilename = $h
 
 [string]$basePath = Get-ScriptDirectory
-[string]$inputPath = '{0}\{1}' -f $basePath, $inputRelPath
-[string]$dcPath = '{0}\{1}' -f $basePath, $dcRelPath
+
+[string]$dcPath = '{0}\{1}' -f $basePath, $dc
+[string]$inputPath = '{0}\{1}' -f $basePath, $target
 [string]$xmlPath = '{0}\{1}' -f $basePath, $xmlFilename
 [string]$htmlPath = '{0}\{1}' -f $basePath, $htmlFilename
+[string]$suppressPath = '{0}\{1}' -f $basePath, $suppressFilename
 
-[string]$scanArgs = '-a "VulnerabilityScan" -s "{0}" -o "{1}" -f "XML"' -f $inputPath, $xmlPath
-[string]$artifactArgs = '-a "VulnerabilityScan" -s "{0}" -o "{1}" -f "HTML"' -f $inputPath, $htmlPath
-[string]$deleteCommand = 'DEL {0}' -f $xmlPath
+if (Test-Path $suppressPath) {
+	[string]$scanArgs = '-a "VulnerabilityScan" -s "{0}" -o "{1}" -f "XML" --suppression "{2}"' -f $inputPath, $xmlPath, $suppressPath
+	[string]$artifactArgs = '-a "VulnerabilityScan" -s "{0}" -o "{1}" -f "HTML" --suppression "{2}"' -f $inputPath, $htmlPath, $suppressPath
+} else {
+	[string]$scanArgs = '-a "VulnerabilityScan" -s "{0}" -o "{1}" -f "XML"' -f $inputPath, $xmlPath
+	[string]$artifactArgs = '-a "VulnerabilityScan" -s "{0}" -o "{1}" -f "HTML"' -f $inputPath, $htmlPath
+}
 
-executeDependencyCheck $dcPath $scanArgs
+executeDependencyCheck $java $dcPath $scanArgs
 
 if (Test-Path $xmlPath) {
 	Write-Output ("Parsing XML output: {0}" -f $xmlPath)
@@ -136,7 +166,7 @@ if (Test-Path $xmlPath) {
 
 if (!$xml.analysis) {
 	Write-Error "XML contains no analysis"
-	Invoke-Expression $deleteCommand
+	Invoke-Expression ('DEL {0}' -f $xmlPath)
 	exit(1)
 }
 
@@ -144,7 +174,7 @@ $dependencies = $xml.analysis.dependencies.dependency
 
 if (!$dependencies) {
 	Write-Error "Analysis contains no dependencies"
-	Invoke-Expression $deleteCommand
+	Invoke-Expression ('DEL {0}' -f $xmlPath)
 	exit(0)
 }
 
@@ -152,11 +182,11 @@ Set-PSConsole
 
 parseDependencies $dependencies
 
-Invoke-Expression $deleteCommand
+Invoke-Expression ('DEL {0}' -f $xmlPath)
 
 if (hasVulnerability $dependencies) {
 	Write-Output ("Vulnerability found -- generating report artifact: {0}" -f $htmlFilename)
-	executeDependencyCheck $dcRelPath $artifactArgs
+	executeDependencyCheck $java $dcPath $artifactArgs
 }
 
 exit(0)
